ea36e3bc5b65321ddbe14d0d1c79eb1773e38990
HSR <hectorsilvarobles@gmail.com>
2020-06-22 13:14:03 -0700

    setting up routes and compnents

---
+++ src/Components/StepForm/StepForm.js
@@ -0,0 +1,22 @@
+import React, {Component} from 'react';
+import './stepform.css'
+
+export class StepForm extends Component {
+    constructor(props){
+        super(props)
+
+        this.state = {
+
+        }
+    }
+
+    render(){
+        return (
+            <div>
+                Step Form
+            </div>
+        )
+    }
+}
+
+export default StepForm
