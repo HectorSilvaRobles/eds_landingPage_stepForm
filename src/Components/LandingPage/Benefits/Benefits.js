963e99e668960e2da7a6101e389683b3772bc62c
hsr <hectorsilvarobles@gmail.com>
2020-09-25 12:45:10 -0700

    setting up the landing pagee

---
+++ src/Components/LandingPage/Benefits/Benefits.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './benefits.css'
+
+const Benefits = () => {
+    return (
+        <div className='benefits'>
+
+        </div>
+    )
+}
+
+export default Benefits
